38ab0744dbe382d2b8c9d83733c535a475d02b52
YounasC <149719453+YounasC@users.noreply.github.com>
2025-11-08 20:42:43 +0000

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -120,29 +120,34 @@ function App() {
       </div> 
 
 
-        {searchHistory.length > 0 && (
-      <div className='absolute top-40 right-8 w-64 bg-white/80 rounded-xl shadow p-4'>
-            <h4 className='font-semibold text-gray-700 mb-4'>Recent locations</h4>
-            <ul className='space-y-2'>
-              {searchHistory.map((item, i) => (
-                <li className='w-full px-4 py-2 bg-blue-100 text-blue-700 rounded-lg hover:bg-blue-200 active:bg-blue-300 transition font-medium shadow-sm'
-                  key={i}>
-                  <button onClick = {() =>
-                  handleSearch(item)}>{item}</button>
+      {searchHistory.length > 0 && (
+    <div className='relative md:absolute md:top-32 md:right-8 w-full md:w-64 bg-white/80 rounded-xl shadow p-4 mt-6 md:mt-0'>
+      <h4 className='font-semibold text-gray-700 mb-4'>Recent locations</h4>
+      <ul className='space-y-2'>
+        {searchHistory.map((item, i) => (
+          <li 
+            key={i}
+            className='w-full px-4 py-2 bg-blue-100 text-blue-700 rounded-lg hover:bg-blue-200 active:bg-blue-300 transition font-medium shadow-sm'
+          >
+            <button onClick={() => handleSearch(item)}>{item}</button>
+          </li>
+        ))}
+      </ul>
+      <button 
+        className='text-red-500 hover:text-red-600 mt-4' 
+        onClick={() => { 
+          setSearchHistory([]);
+          localStorage.removeItem("searchHistory");
+          setWeather(null);
+          setCity("");
+        }}
+      >
+        Clear History
+      </button>
+    </div>
 
-                </li>
-              ))}
-            </ul>
-            <button className='text-red-500 hover:text-red-600 mt-4' onClick={() => { setSearchHistory([]);
-            localStorage.removeItem("searchHistory");
-            setWeather(null);
-            setCity("");
-            
-            }}
-            >
-            Clear History
-          </button>
-      </div>
+      
+      
       )}
       <Weather weather={weather}/>
 
